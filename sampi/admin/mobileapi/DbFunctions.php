ddfd80b821021e74360de7e78b08c9bcc3e2a6f5
SvenDub <sven.dubbeld1@gmail.com>
2013-10-12 20:28:12 +0200

    Fix adding posts via mobile API

--- sampi/admin/mobileapi/DbFunctions.php
+++ sampi/admin/mobileapi/DbFunctions.php
@@ -25,4 +25,44 @@ require_once SampiCMS\ROOT . '/sampi/functions.php';
  *
  */
 class DbFunctions extends SampiCMS\DbFunctions {
+	/**
+	 * Add a new post to the database
+	 *
+	 * @param string $title
+	 *        	Title of the post
+	 * @param string $content
+	 *        	Content of the post
+	 * @param string $username
+	 *        	Username of the poster.
+	 *        	Used in combination with $password to authenticate the poster.
+	 * @param string $password
+	 *        	Password of the poster.
+	 *        	Used in combination with $username to authenticate the poster.
+	 * @return boolean True on success, false on failure
+	 *
+	 * @see \SampiCMS\Post
+	 */
+	function newPost($title, $content, $keywords, $username, $password) {
+		if ($title !== "" && $content !== "") {
+			if (SampiCMS\DbFunctions::checkAuth($username, $password)) {
+				$date = date ( 'Y-m-d H:i:s' );
+				$dateUpdated = date ( 'Y-m-d H:i:s' );
+				$stmt = $this->con->prepare( "INSERT INTO sampi_posts (date, date_updated, author, title, content, keywords) VALUES (?, ?, ?, ?, ?, ?)" );
+				$stmt->bind_param('ssssss', $date, $dateUpdated, $username, $title, $content, $keywords);
+				$stmt->execute();
+				if ($stmt->affected_rows > 0) {
+					$stmt->free_result();
+					$stmt->close();
+					return true;
+				} else {
+					$stmt->free_result();
+					$stmt->close();
+					return false;
+				}
+			} else {
+				
+				return false;
+			}
+		}
+	}
 }
